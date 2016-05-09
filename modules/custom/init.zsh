1571c01a4663ffcdf7f97ea98bb65a68a78cc396
Hugues Lismonde <hugues@epic.net>
2016-05-09 09:52:25 +0200

    Remove GO15VENDOREXPERIMENT, add d alias

--- modules/custom/init.zsh
+++ modules/custom/init.zsh
@@ -14,6 +14,7 @@ fi
 # any custom stuff should go here.
 # ensure that 'custom' exists in the zmodules array in your .zimrc
 
+alias d='dirs -v | head -10'
 alias dh='dirs -v'
 alias gst=gwS
 alias gup='git up'
